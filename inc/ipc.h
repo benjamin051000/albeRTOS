c333b524e546e1b5d9cdf38f8848c670aaf7d99f
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2024-08-15 02:29:48 -0400

    updated fifo to a class

--- inc/ipc.h
+++ inc/ipc.h
@@ -1,107 +1,85 @@
 #pragma once
 
+#include <cstdint>
 #include <semaphores.h>
 
 namespace albeRTOS {
 
-template<typename T, int MAXSIZE>
-class FIFO {
-    T buffer[MAXSIZE]; // Data buffer // TODO replace with std::array for nicer api
-    T *head, *tail;
-    unsigned lostData; // Counts amount of lost data
+/**
+ * A FIFO.
+ * Underlying implementation is a circular buffer of size MAXSIZE.
+ */
+template<typename Type, int32_t MAXSIZE> // TODO what type should MAXSIZE be?
+class fifo {
+    Type buffer[MAXSIZE]; // Data buffer // TODO replace with std::array for nicer api
+    Type *head, *tail;
+    int32_t lostData; // Counts amount of lost data // TODO how large should this type be?
 	Semaphore currentSize, mutex;
 public:
     // Constructor
-    FIFO();
+	fifo() : head(&buffer[0]), tail(&buffer[0]), lostData(0) {
+		initSemaphore(currentSize, 0);
+		initSemaphore(mutex, 1);
+	}
 
-    T read();
-    bool write(T data);
+	/*
+	 * Reads fifo
+	 *  - Waits until CurrentSize semaphore is greater than zero
+	 *  - Gets data and increments the head ptr (wraps if necessary)
+	 * Param: "FIFOChoice": chooses which buffer we want to read from
+	 * Returns: uint32_t Data from fifo
+	 */
+	Type read() {
+		waitSemaphore(currentSize); // block if empty
+		waitSemaphore(mutex); //in case something else is reading
+		const Type data = *head;
 
-    int32_t size() const {return currentSize;}
-    bool full() const {return currentSize == MAXSIZE;}
+		if(head == &buffer[MAXSIZE-1]) {
+			head = &buffer[0]; //overflow if necessary
+		}
+		else {
+			head++;
+		}
 
+		signalSemaphore(mutex);
+		return data;
+	}
+
+	/*
+	 *  Writes data to Tail of the buffer if the buffer is not full
+	 *  Increments tail (wraps if necessary)
+	 *  Param "FIFOIndex": chooses which buffer to read from
+	 *        "Data": Data being put into fifo
+	 *  Returns: false for full buffer (unable to write), true if no errors
+	 */
+	bool write(Type data) {
+		waitSemaphore(mutex);
+
+		if(currentSize == MAXSIZE) { // Out of room
+			lostData++;
+			signalSemaphore(mutex);
+			return false;
+		}
+		else {
+			*tail = data; //write data // TODO verify this ref drop-in works since there's a weird deref
+
+			if(tail == &buffer[MAXSIZE-1]) {
+
+				tail = &buffer[0];
+			}
+			else {
+				tail++;
+			}
+		}
+
+		signalSemaphore(currentSize); // Increase the size! Also unblocks threads that were waiting for an element.
+
+		signalSemaphore(mutex);
+		return true;
+	}
+
+    [[nodiscard]] int32_t size() const {return currentSize;}
+    [[nodiscard]] bool full() const {return currentSize == MAXSIZE;}
 };
-
-
-template<typename T, int MAXSIZE>
-FIFO<T, MAXSIZE>::FIFO() {
-    head = &buffer[0];
-    tail = &buffer[0];
-    lostData = 0;
-	initSemaphore(currentSize, 0);
-    initSemaphore(mutex, 1);
-}
-
-
-/*
- * Reads FIFO
- *  - Waits until CurrentSize semaphore is greater than zero
- *  - Gets data and increments the head ptr (wraps if necessary)
- * Param: "FIFOChoice": chooses which buffer we want to read from
- * Returns: uint32_t Data from FIFO
- */
-template<typename T, int MAXSIZE>
-T FIFO<T, MAXSIZE>::read() {
-    T data;
-
-    waitSemaphore(currentSize); // block if empty
-    waitSemaphore(mutex); //in case something else is reading
-    data = *head;
-
-    if(head == &buffer[MAXSIZE-1]) {
-        head = &buffer[0]; //overflow if necessary
-    }
-    else {
-        head++;
-    }
-
-    signalSemaphore(mutex);
-    return data;
-}
-
-/*
- *  Writes data to Tail of the buffer if the buffer is not full
- *  Increments tail (wraps if necessary)
- *  Param "FIFOIndex": chooses which buffer to read from
- *        "Data": Data being put into FIFO
- *  Returns: false for full buffer (unable to write), true if no errors
- */
-template<typename T, int MAXSIZE>
-bool FIFO<T, MAXSIZE>::write(T data) {
-    waitSemaphore(mutex);
-
-    if(currentSize == MAXSIZE) { // Out of room
-        lostData++;
-        signalSemaphore(mutex);
-        return false;
-    }
-    else {
-        *tail = data; //write data // TODO verify this ref drop-in works since there's a weird deref
-
-        if(tail == &buffer[MAXSIZE-1]) {
-
-            tail = &buffer[0];
-        }
-        else {
-            tail++;
-        }
-    }
-
-    signalSemaphore(currentSize); // Increase the size! Also unblocks threads that were waiting for an element.
-
-    signalSemaphore(mutex);
-    return true;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// Old FIFO stuff
-////////////////////////////////////////////////////////////////////////////////
-/*
- * Initializes One to One FIFO Struct
- */
-int initFIFO(uint32_t FIFOIndex);
-
-int32_t readFIFO(unsigned FIFOIndex);
-int writeFIFO(unsigned FIFOIndex, int32_t data);
 
 } // end of namespace albeRTOS
