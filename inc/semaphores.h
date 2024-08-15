a7499a142b9e1941518ff24f33608f1f461267b4
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2024-08-15 02:30:10 -0400

    updated semaphores to a class.

--- inc/semaphores.h
+++ inc/semaphores.h
@@ -5,28 +5,12 @@
 
 namespace albeRTOS {
 
-using Semaphore = std::int32_t;
+class Semaphore {
+	int32_t value;
+public:
+	Semaphore(const int32_t value);
+	void wait();
+	void signal();
+};
 
-/*
- * Initializes a semaphore to a given value
- * Param "s": Pointer to semaphore
- * Param "value": Value to initialize semaphore to
- */
-void initSemaphore(Semaphore &s, int32_t value);
-
-/*
- * Waits for a semaphore to be available (value greater than 0)
- * 	- Decrements semaphore when available
- * 	- Spinlocks to wait for semaphore
- * Param "s": Pointer to semaphore to wait on
- */
-void waitSemaphore(Semaphore &s);
-
-/*
- * Signals the completion of the usage of a semaphore
- * 	- Increments the semaphore value by 1
- * Param "s": Pointer to semaphore to be signalled
- */
-void signalSemaphore(Semaphore &s);
-
-} // end of namespace albertOS
+} // end of namespace albeRTOS
--- src/semaphores.cpp
+++ src/semaphores.cpp
@@ -3,28 +3,28 @@
 #include <criticalsection.h>
 #include <scheduler.h>
 
-extern void contextSwitch();
+namespace albeRTOS {
 
-void albeRTOS::initSemaphore(Semaphore& s, int32_t value) {
-    START_CRIT_SECTION;
-	s = value;
+Semaphore::Semaphore(const int32_t value) {
+	START_CRIT_SECTION;
+	// TODO check that value is >= 0 ?
+	this->value = value;
 	END_CRIT_SECTION;
 }
 
+void Semaphore::wait() {
+	START_CRIT_SECTION;
 
-void albeRTOS::waitSemaphore(Semaphore& s) {
-    START_CRIT_SECTION;
+	value -= 1; // declare ownership
 
-    s--; // declare ownership
+	// If s < 0, it's currently claimed by another thread.
+	if(value < 0) {
+		currentThread->blocked = this; // block this thread
+		END_CRIT_SECTION;
+		contextSwitch(); // do something else until this thread is unblocked (other thread releases the Semaphore)
+	}
 
-    // If s < 0, it's currently claimed by another thread.
-    if(s < 0) {
-        currentThread->blocked = &s; // block this thread
-        END_CRIT_SECTION;
-        contextSwitch(); // do something else until this thread is unblocked (other thread releases the Semaphore)
-    }
-
-    END_CRIT_SECTION;
+	END_CRIT_SECTION;
 }
 
 /*
@@ -34,31 +34,33 @@ void albeRTOS::waitSemaphore(Semaphore& s) {
  * Param "s": Pointer to semaphore to be signaled
  * THIS IS A CRITICAL SECTION
  */
-void albeRTOS::signalSemaphore(Semaphore& s) {
-    START_CRIT_SECTION;
+void Semaphore::signal() {
+	START_CRIT_SECTION;
 
-    s++;
+	value += 1;
 
-    // If value leq 0, other threads are blocking for this resource.
-    if(s <= 0) {
+	// If value leq 0, other threads are blocking for this resource.
+	if(value <= 0) {
 
-        const TCB * const curr_thread_temp = currentThread;
-        TCB* thread = currentThread->next;
+		const TCB * const curr_thread_temp = currentThread;
+		TCB* thread = currentThread->next;
 
-        // Iterate through each thread.
-        while(thread != curr_thread_temp) {
-            if(thread->blocked == &s) // If the thread is blocked by this Semaphore,
-                thread->blocked = 0; // Unblock it.
+		// Iterate through each thread.
+		while(thread != curr_thread_temp) {
+			if(thread->blocked == this) // If the thread is blocked by this Semaphore,
+				thread->blocked = 0; // Unblock it.
 
-            thread = thread->next;
-        }
+			thread = thread->next;
+		}
 
-//        while(blocked_thread->blocked != &s) { // unblock one
-//            blocked_thread = blocked_thread->next;
-//        }
-//
-//        // blocked_thread->blocked == &s, so blocked_thread is blocked by this semaphore!
-//        blocked_thread->blocked = nullptr; //make it unblocked
-    }
-    END_CRIT_SECTION;
+		//        while(blocked_thread->blocked != &s) { // unblock one
+		//            blocked_thread = blocked_thread->next;
+		//        }
+		//
+		//        // blocked_thread->blocked == &s, so blocked_thread is blocked by this semaphore!
+		//        blocked_thread->blocked = nullptr; //make it unblocked
+	}
+	END_CRIT_SECTION;
 }
+
+} // end of namespace albeRTOS
